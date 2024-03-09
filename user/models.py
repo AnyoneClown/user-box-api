ef5b83b1aa880ac51b9bc3ee7461e45a886c99c7
Denis Dymerlii <dum.denya15@gmail.com>
2024-03-09 10:15:29 +0200

    make email field as username field, register admin model

--- user/models.py
+++ user/models.py
@@ -1,6 +1,41 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.core.validators import RegexValidator
 from django.db import models
+from django.utils.translation import gettext as _
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
 
 
 class User(AbstractUser):
@@ -14,6 +49,8 @@ class User(AbstractUser):
         UKRAINE = "Ukraine"
         FRANCE = "France"
 
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
     telegram_id = models.IntegerField(max_length=20, unique=True)
     telegram_username = models.CharField(max_length=50, unique=True)
     phone_regex = RegexValidator(
@@ -24,6 +61,11 @@ class User(AbstractUser):
     language = models.CharField(choices=LangaugeChoices.choices, max_length=50)
     country = models.CharField(choices=CountyChoices.choices, max_length=50)
     verification_code = models.CharField(max_length=6)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
 
     class Meta:
         ordering = ["telegram_username"]
