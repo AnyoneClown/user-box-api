92d3a63bae1ee394c43f43e6df626e5cf794d8b2
Denis Dymerlii <dum.denya15@gmail.com>
2024-03-10 18:34:01 +0200

    register cabinet urls

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/users/", include("user.urls", namespace="users")),
-
+    path("api/cabinet/", include("cabinet.urls", namespace="cabinet"))
 ]
