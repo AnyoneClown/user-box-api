ec125abd0edf1d1d3925821597ed3dc1769a81b7
Denis Dymerlii <dum.denya15@gmail.com>
2024-03-11 12:20:35 +0200

    add 3 routers to urls

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from rest_framework import routers
 
-from cabinet.views import BoxViewSet, NotificationViewSet, OperationViewSet, CoinViewSet
+from cabinet.views import BoxViewSet, NotificationViewSet, OperationViewSet, CoinViewSet, UserNotificationViewSet, UserBalanceViewSet, UserBoxViewSet
 
 router = routers.DefaultRouter()
 router.register("boxes", BoxViewSet)
 router.register("notifications", NotificationViewSet)
 router.register("operations", OperationViewSet)
 router.register("coins", CoinViewSet)
+
+router.register("user-boxes", UserBoxViewSet)
+router.register("user-notifications", UserNotificationViewSet)
+router.register("user-balances", UserBalanceViewSet)
+
 
 urlpatterns = [] + router.urls
 
